19041361b5179d2a322fec6ee423e003f48966e5
xiasun <xiasun@xiasuns-MacBook-Pro-2.local>
2023-07-17 19:39:51 -0700

    add questions.json as example

--- src/index.js
+++ src/index.js
@@ -1,61 +1,35 @@
 const XLSX = require("xlsx");
+const fs = require("fs").promises;
 
-function convertJsonToXlsx() {
-  // Assume your json data is something like this:
-  const data = [
-    {
-      questions: "q-category",
-      "max-selections": "3",
-      "min-selections": "1",
-    },
-    {
-      questions: "q-rather",
-      "max-selections": "1",
-      "min-selections": "1",
-    },
-    {
-      questions: "q-video",
-      "max-selections": "1",
-      "min-selections": "1",
-    },
-    {
-      questions: "q-photo",
-      "max-selections": "1",
-      "min-selections": "1",
-    },
-    {
-      questions: "q-customer",
-      "max-selections": "1",
-      "min-selections": "1",
-    },
-    {
-      questions: "q-3d",
-      "max-selections": "1",
-      "min-selections": "1",
-    },
-    {
-      questions: "q-pdf",
-      "max-selections": "1",
-      "min-selections": "1",
-    },
-    {
-      questions: "q-design",
-      "max-selections": "1",
-      "min-selections": "1",
-    },
-    {
-      questions: "q-illustration",
-      "max-selections": "1",
-      "min-selections": "1",
-    },
-  ];
-
-  const ws = XLSX.utils.json_to_sheet(data);
-  const wb = XLSX.utils.book_new();
-  XLSX.utils.book_append_sheet(wb, ws, "helix-questions");
-
-  // Write workbook to disk
-  XLSX.writeFile(wb, "output.xlsx");
+async function readJsonFile(filepath) {
+  try {
+    const originData = await fs.readFile(filepath, "utf8");
+    return JSON.parse(originData);
+  } catch (err) {
+    console.error("Error reading file:", err);
+    return null;
+  }
 }
 
-convertJsonToXlsx();
+function convertJsonToXlsx(jsonData, outputFileName) {
+  const wb = XLSX.utils.book_new();
+  
+  for (let key in jsonData) {
+    if (!key.startsWith(":")) {
+      const ws = XLSX.utils.json_to_sheet(jsonData[key].data);
+      XLSX.utils.book_append_sheet(wb, ws, "helix-" + key);
+    }
+  }
+
+  // Write workbook to disk
+  XLSX.writeFile(wb, outputFileName);
+}
+
+async function main() {
+  const jsonData = await readJsonFile("questions.json");
+  if(jsonData !== null) {
+    convertJsonToXlsx(jsonData, "questions.xlsx");
+  }
+}
+
+main();
