9515617cc47da1e4156af18d4ed9ded975a5901b
xiasun <xiasun@xiasuns-MacBook-Pro-2.local>
2023-07-17 17:31:38 -0700

    use question json for example

--- src/index.js
+++ src/index.js
@@ -1,18 +1,61 @@
-const XLSX = require('xlsx');
+const XLSX = require("xlsx");
 
 function convertJsonToXlsx() {
   // Assume your json data is something like this:
   const data = [
-    {name: 'John', age: 30, city: 'New York'},
-    {name: 'Jane', age: 40, city: 'Chicago'},
+    {
+      questions: "q-category",
+      "max-selections": "3",
+      "min-selections": "1",
+    },
+    {
+      questions: "q-rather",
+      "max-selections": "1",
+      "min-selections": "1",
+    },
+    {
+      questions: "q-video",
+      "max-selections": "1",
+      "min-selections": "1",
+    },
+    {
+      questions: "q-photo",
+      "max-selections": "1",
+      "min-selections": "1",
+    },
+    {
+      questions: "q-customer",
+      "max-selections": "1",
+      "min-selections": "1",
+    },
+    {
+      questions: "q-3d",
+      "max-selections": "1",
+      "min-selections": "1",
+    },
+    {
+      questions: "q-pdf",
+      "max-selections": "1",
+      "min-selections": "1",
+    },
+    {
+      questions: "q-design",
+      "max-selections": "1",
+      "min-selections": "1",
+    },
+    {
+      questions: "q-illustration",
+      "max-selections": "1",
+      "min-selections": "1",
+    },
   ];
 
   const ws = XLSX.utils.json_to_sheet(data);
   const wb = XLSX.utils.book_new();
-  XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+  XLSX.utils.book_append_sheet(wb, ws, "helix-questions");
 
   // Write workbook to disk
-  XLSX.writeFile(wb, 'output.xlsx');
+  XLSX.writeFile(wb, "output.xlsx");
 }
 
 convertJsonToXlsx();
