ba0f3db045b48b4b4775a92017f22b99a954fa83
xiasun <xiasun@xiasuns-MacBook-Pro-2.local>
2023-07-18 15:30:40 -0700

    add git diff files logic

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const XLSX = require("xlsx");
 const fs = require("fs").promises;
+const simpleGit = require("simple-git");
 
 async function readJsonFile(filepath) {
   try {
@@ -13,7 +14,7 @@ async function readJsonFile(filepath) {
 
 function convertJsonToXlsx(jsonData, outputFileName) {
   const wb = XLSX.utils.book_new();
-  
+
   for (let key in jsonData) {
     if (!key.startsWith(":")) {
       const ws = XLSX.utils.json_to_sheet(jsonData[key].data);
@@ -25,11 +26,35 @@ function convertJsonToXlsx(jsonData, outputFileName) {
   XLSX.writeFile(wb, outputFileName);
 }
 
+async function getLatestCommitFiles() {
+  const git = simpleGit();
+  const logList = await git.log();
+  const latestCommitHash = logList.latest.hash;
+  const diffSummary = await git.diffSummary([
+    `${latestCommitHash}~1..${latestCommitHash}`,
+  ]);
+
+  console.log("Files changed in the latest commit:");
+  diffSummary.files.forEach((file) => console.log(file.file));
+
+  return diffSummary.files;
+}
+
 async function main() {
-  const jsonData = await readJsonFile("questions.json");
-  if(jsonData !== null) {
-    convertJsonToXlsx(jsonData, "questions.xlsx");
+  const files = await getLatestCommitFiles();
+
+  for (let file of files) {
+    if (file.file.endsWith(".json")) {
+      const jsonData = await readJsonFile(file.file);
+      if (jsonData !== null) {
+        convertJsonToXlsx(jsonData, file.file.replace("json", "xlsx"));
+      }
+    }
   }
+  // const jsonData = await readJsonFile("questions.json");
+  // if(jsonData !== null) {
+  //   convertJsonToXlsx(jsonData, "questions.xlsx");
+  // }
 }
 
 main();
